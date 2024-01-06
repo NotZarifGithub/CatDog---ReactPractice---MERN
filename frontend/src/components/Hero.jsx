27d66508ee41ac2a97dcec38b654ccef7a8548ea
NotZarif <flacko.programming@gmail.com>
2024-01-06 17:25:14 +0800

    hero section done

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -2,21 +2,24 @@ import Button from "./Button";
 
 const Hero = () => {
   return (
-    <section>
-      <div>
-        A place where you can find a list full of information on cats and dogs
-        breed.
-      </div>
+    <section className="p-[20px]  max-w-[1000px] ">
+      <section className="border flex flex-col  gap-6 text-center border-black rounded-3xl md:rounded-[100px] p-[30px] md:p-[60px] lg:p-[90px] shadow-xl drop-shadow-xl">
+        <div className="text-3xl font-semibold md:text-5xl md:font-bold lg:text-6xl lg:font-black">
+          A place where you can find a list full of information on cats and dogs
+          breed.
+        </div>
 
-      <div>
-        Explore a curated list of cat and dog breeds, providing essential
-        information at your fingertips. Find your perfect furry match with ease,
-        discovering key traits and care insights in a streamlined format.
-      </div>
+        <div className="md:text-lg lg:text-xl">
+          Explore a curated list of cat and dog breeds, providing essential
+          information at your fingertips. Find your perfect furry match with
+          ease, discovering key traits and care insights in a streamlined
+          format.
+        </div>
 
-      <div className="">
-        <Button />
-      </div>
+        <div className="">
+          <Button />
+        </div>
+      </section>
     </section>
   );
 };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Hero from "../components/Hero";
 const Home = () => {
   return (
     <main className="flex justify-between items-center max-w-[1200px] mx-auto ">
-      <section>
+      <section className="mx-auto py-[40px] md:py-[80px]">
         <Hero />
       </section>
     </main>
